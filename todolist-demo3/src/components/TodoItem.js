79709cdc48809faf9545d21864ed123a893a2115
zxymax <497084653@qq.com>
2021-12-11 16:32:40 +0800

    PropTypes的用法

--- todolist-demo3/src/components/TodoItem.js
+++ todolist-demo3/src/components/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes  from 'prop-types'
 
 class TodoItem extends Component {
   constructor(props) {
@@ -13,8 +14,19 @@ class TodoItem extends Component {
   }
 
   render() {
-    return <li onClick={this.handleClick}>{this.props.content}</li>
+    return <li onClick={this.handleClick}>{this.props.week}-{this.props.content}</li>
   }
 }
 
+TodoItem.propTypes = {
+  content: PropTypes.string.isRequired
+}
+
+// week父组件没有传 默认传值
+TodoItem.defaultProps = {
+  week: 'sunday'
+}
+
+
 export default TodoItem
+
