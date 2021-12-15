5b938e660e17edd38461f61657e5bf628913253a
zxymax <497084653@qq.com>
2021-12-15 12:17:35 +0800

    UI组件和容器组件拆分 无状态组件的使用 props使用去掉this

--- todolist-nostatuscomponents/src/TodoListUI.js
+++ todolist-nostatuscomponents/src/TodoListUI.js
@@ -1,41 +1,35 @@
 import React, { Component } from "react";
 import { Input, Button, List } from "antd";
 
-class TodoListUI extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <Input
-          placeholder="Basic usage"
-          style={{ width: "300px", marginRight: "5px" }}
-          value={this.props.inputValue}
-          onChange={this.props.handleChange}
-        />
-        <Button type="primary" onClick={this.props.handleClick}>
-          Add information
-        </Button>
-        <List
-          size="large"
-          style={{ width: "300px", marginTop: "5px" }}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item
-              onClick={(index) => {
-                this.props.handleDeleteItem(index);
-              }}
-            >
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
-    );
-  }
-}
+const TodoListUI = (props) => {
+  return (
+    <div>
+      <Input
+        placeholder="Basic usage"
+        style={{ width: "300px", marginRight: "5px" }}
+        value={props.inputValue}
+        onChange={props.handleChange}
+      />
+      <Button type="primary" onClick={props.handleClick}>
+        Add information
+      </Button>
+      <List
+        size="large"
+        style={{ width: "300px", marginTop: "5px" }}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item
+            onClick={(index) => {
+              props.handleDeleteItem(index);
+            }}
+          >
+            {item}
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
 export default TodoListUI;
